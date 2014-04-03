f6adbcd693de0206a49be517e8c5f45f34767ba2
blackout314 <blackout314@vm-0.blackout314.koding.kd.io>
2014-04-03 15:42:18 +0000

    others method

--- class.pkanbanize.php
+++ class.pkanbanize.php
@@ -1,6 +1,6 @@
 <?php
 
-/*
+/**
  * pkanbanize class v2.0
  */
 
@@ -71,7 +71,6 @@ namespace Lib;
 /**
  * pkanbanize
  *
- * 
  * $k = new \Lib\pkanbanize( YOURKEY ); || $k = new \Lib\pkanbanize( null,null, USER,PASS );
  * $task = $k->getAllTasks(6);
  * echo print_r($task['res']);
@@ -86,7 +85,6 @@ class pkanbanize {
     protected $_url = 'kanbanize.com/index.php/api/kanbanize';
     
     public function __construct ($key=null, $domain=null, $mail=null, $pass=null) {
-        
         if ($key) {
             $this->_key = $key;
         }
@@ -105,6 +103,7 @@ class pkanbanize {
             $this->API->setKey( $this->_key );
         }
     }
+    // private __login function
     private function __login () {
         $this->API->data(array('email' => $this->_mail, 'pass' => $this->_pass));
         return $this->API->call('login');
@@ -121,7 +120,131 @@ class pkanbanize {
 		$this->API->data(array('boardid' => $boardid));
 		return $this->API->call('get_all_tasks');
 	}
-    
+
+	/**
+	 * @name getBoardActivities
+	 *
+	 * @param {int} boardid
+	 * @param {string} fromdate
+	 * @param {string} todate
+	 * @param {int} [page] default 1
+	 * @param {int} [resultsperpage] default 30
+	 * @param {string} [author] default ALL
+	 * @param {string} [eventtype] Transitions, Updates, Comments, Blocks. Default is ALL
+	 * @param {string} [textformat] "plain" (default) and "html". If the plain text format is used, the HTML tags are stripped from the history details
+	 *
+	 * @return {Array} all activities 
+	 * - allactivities : The number of all activities for the corresponding time window specified by the fromdate and todate parameters.
+	 * - page : current page
+	 * - activities[] : author, event, text, date, taskid
+	 */
+	public function getBoardActivities ($boardid, $fromdate, $todate, $page=1, $resultsperpage=30, $author=null, $eventtype=null, $textformat='plain') {
+		$this->API->data( array(
+			'boardid' => $boardid, 
+			'fromdate' => $fromdate, 
+			'todate' => $todate,
+			'page' => $page,
+			'resultsperpage' => $resultsperpage,
+			'author' => $author,
+			'eventtype' => $eventtype,
+			'textformat' => $textformat
+		) );
+		return $this->API->call('get_board_activities');
+	}
+
+	/**
+	 * @name getTaskDetails
+	 *
+	 * @param int  $boardid The ID of the board you want the new task created into. You can see the board ID on the dashboard screen, in the upper right corner of each board.
+	 * @param int  $taskid The ID of the task to be deleted.
+	 * @param bool $history if you want to get history for the task.
+	 *
+	 * @return array
+	 */
+	public function getTaskDetails ($boardid, $taskid, $history = false) {
+		$this->API->data(array(
+		    'boardid' => $boardid, 
+		    'taskid' => $taskid,
+		    'history' => $history ? 'yes' : false
+        ));
+		return $this->API->call('get_task_details');
+	}
+
+	/**
+	 * @name getBoardSettings
+	 * @desc get_board_settings method. Limit 30/hour
+	 *
+	 * @param int $boardid The ID of the board you want the new task created into. You can see the board ID on the dashboard screen, in the upper right corner of each board.
+	 *
+	 * @return array
+	 *
+	 * - usernames Array containing the usernames of the board members.
+	 * - templates Array containing the templates available to this board.
+	 * - types     Array containing the types available to this board.
+	 */
+	public function getBoardSettings ($boardid) {
+		$this->API->data(array('boardid' => $boardid));
+		return $this->API->call('get_board_settings');
+	}
+
+	/**
+	 * @name getBoardStructure
+	 * @desc get_board_structure method. Limit 30/hour
+	 *
+	 * @param int $boardid The ID of the board you want the new task created into. You can see the board ID on the dashboard screen, in the upper right corner of each board.
+	 *
+	 * @return array
+	 *
+	 * - columns    Array containing the board columns (only the columns on last level are returned)
+	 * - columns[][position]       The position of the column
+	 * - columns[][lcname]         The name of the column.
+	 * - columns[][description]    The description of the column or swimlane.
+	 * - lanes      Array containing the board swimnales.
+	 * - lanes[][lcname]         The name of the swimlane.
+	 * - lanes[][color]          The color of the swimlane.
+	 * - lanes[][description]    The description of the column or swimlane.
+	 */
+	public function getBoardStructure ($boardid) {
+        $this->API->data(array('boardid' => $boardid));
+		return $this->API->call('get_board_structure');
+	}
+
+	/**
+	 * @name editTask
+	 *
+	 * @param int   $boardid The ID of the board where the task to be moved is located. You can see the board ID on the dashboard screen, in the upper right corner of each board.
+	 * @param int   $taskid The ID of the task to be deleted.
+	 *
+	 * @param array $changeData
+	 *
+	 * - title         Title of the task
+	 * - description Description of the task
+	 * - priority    One of the following: Low, Average, High
+	 * - assignee    Username of the assignee (must be a valid username)
+	 * - color       Any color code (e.g. 99b399) DO NOT PUT the # sign in front of the code!!!
+	 * - size        Size of the task
+	 * - tags        Space separated list of tags
+	 * - deadline    Dedline in the format: yyyy-mm-dd (e.g. 2011-12-13)
+	 * - extlink     A link in the following format: https:\\github.com\philsturgeon. If the parameter is embedded in the request BODY
+	 * - type        The name of the type you want to set.
+	 *
+	 * @return 1|string|null
+	 */
+	public function editTask($boardid, $taskid, $changeData = array()) {
+		$d = array('boardid' => $boardid, 'taskid' => $taskid);
+		foreach ($changeData as $k => $v) {
+			$d[$k] = $v;
+		}
+
+		$this->API->data($d);
+
+		$resp = $this->API->call('edit_task');
+		if ($resp) {
+			return $resp['code'] ? : null;
+		}
+		return null;
+	}
+
 }
 
 ?>
